b709f85919cc3dc33810c772f2e6577fe055cd35
andygavrilov <volirvag132005@bk.ru>
2024-05-28 21:23:13 +0300

    GC and JVM

--- src/ru/innopolis/java/first/Main.java
+++ src/ru/innopolis/java/first/Main.java
@@ -4,15 +4,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите имя: ");
-        String name = scanner.nextLine();
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Введите имя: ");
+//        String name = scanner.nextLine();
+//
+//        int start = 1;
+//        int end = 5;
+//
+//        for (int i = start; i <= end; i++) {
+//            System.out.println("Hello, world! Hello, " + name);
+//        }
+        int a = 3;
+        int b = 2;
 
-        int start = 1;
-        int end = 5;
+        System.out.println("Result: " + a + b);
 
-        for (int i = start; i <= end; i++) {
-            System.out.println("Hello, world! Hello, " + name);
-        }
+//        System.out.println(Integer.MIN_VALUE);
+//        System.out.println(Integer.MAX_VALUE);
     }
 }
---
+++ src/ru/innopolis/java/jmm/toread.md
@@ -0,0 +1,8 @@
+* https://habr.com/ru/articles/744834/ - Введение в Java Process Memory Model
+* https://habr.com/ru/articles/549176/ - Управление памятью Java
+* https://habr.com/ru/articles/169869/ - Java 8: от PermGen к MetaSpace
+* https://habr.com/ru/articles/685518/ - Глубокое погружение в Java Memory Model
+* https://habr.com/ru/articles/510454/ - Java-модель памяти (часть 1)
+* https://habr.com/ru/companies/otus/articles/776342/ - Garbage Collection и JVM
+* https://habr.com/ru/companies/otus/articles/658219/ - Сборка мусора в Java
+* https://habr.com/ru/companies/haulmont/articles/591633/ - Прогресс GC от JDK 8 до JDK 17
